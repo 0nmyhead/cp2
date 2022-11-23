aa0479508f63923002a2d65cab4d3383a1b1e2f9
0nmyhead <jg020135@naver.com>
2022-11-23 16:16:09 +0900

    feat/ongoing statement - add sign-in

--- user/models.py
+++ user/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, username, phone, age, password=None):
+
+        if not email:
+            raise ValueError('email must be defined.')
+        if not username:
+            raise ValueError('username must be defined.')
+        
+        user = self.model(email = self.normalize_email(email), username = username, phone = phone, age = age)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, username, phone, age, password=None):
+
+        user = self.create_user(email=email, username=username, phone=phone, age=age, password=password)
+
+        user.is_admin = True
+        user.save(using=self._db)
+
+        return user
+class User(AbstractBaseUser):
+
+    objects = UserManager()
+
+    email = models.EmailField(default='', max_length=100, null=False, blank=False, unique=True)
+    username = models.CharField(default='', max_length=100, null=False, blank=False, unique=True)
+    phone = models.CharField(default='', max_length=30, null=True, blank=True)
+    age = models.IntegerField(default=0, null=True, blank=True)
+
+    is_active = models.BooleanField(default=True)    
+    is_admin = models.BooleanField(default=False)
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+
+    class Meta:
+
+        db_table = 'user'
--- user/views.py
+++ user/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from datetime import datetime
+from .models import User
+from .serializers import RegisterSerializer, SigninSerializer
+from rest_framework import generics
 
-# Create your views here.
+class UserCreate(generics.CreateAPIView):
+
+    serializer_class = RegisterSerializer
+
+    """ def post(self, request):
+
+        serializer_class = RegisterSerializer
+        if serializer_class.is_valid():
+            serializer_class.save(user = self.request.user) """
+
+class UserSignin(generics.CreateAPIView):
+
+    
+    serializer_class = SigninSerializer
+"""     def post(self, request):
+
+        serializer_class = SigninSerializer
+        if serializer_class.is_valid():
+
+            user = serializer_class.validated_data
+ """
+
+    
